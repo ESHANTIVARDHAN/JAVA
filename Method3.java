76256d19e32896eb0e4b7c445145dce3a4799047
ESHANTIVARDHAN <239x1a32c0@gprec.ac.in>
2025-09-02 17:35:22 +0530

    Add files via upload

---
+++ Method3.java
@@ -0,0 +1,13 @@
+package day_2;
+
+public class Method3 {
+	static void vardha(int val) {
+		System.out.println("Parameter and No return value method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vardha(25);
+	}
+
+}
