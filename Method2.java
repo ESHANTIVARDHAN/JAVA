6e602eb89c55e39bd5608a87318e5f51e575a807
ESHANTIVARDHAN <239x1a32c0@gprec.ac.in>
2025-09-02 17:30:52 +0530

    Add files via upload

---
+++ Method2.java
@@ -0,0 +1,11 @@
+package day_2;
+
+public class Method2 {
+	static void hello() {
+		System.out.println("No paramter and No return value Method");
+	}
+	public static void main(String[] args) {
+		hello();
+	}
+	
+}
