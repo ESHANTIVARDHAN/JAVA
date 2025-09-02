0a59cc88ef92430a7239ae48f3b4d6a42cf242d8
ESHANTIVARDHAN <239x1a32c0@gprec.ac.in>
2025-09-02 17:16:13 +0530

    Add files via upload

---
+++ returntypeMethod.java
@@ -0,0 +1,16 @@
+package day_2;
+
+public class returntypeMethod {
+	static String HelloWorld() {
+		System.out.println("HelloWorld");
+		return "String";
+	}
+
+	public static void main(String[] args) {
+		HelloWorld();
+		HelloWorld();
+		// TODO Auto-generated method stub
+
+	}
+
+}
