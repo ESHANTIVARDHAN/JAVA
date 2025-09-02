b32fa9f61e8eff89ea940ac021a1e61abe2cc9b0
ESHANTIVARDHAN <239x1a32c0@gprec.ac.in>
2025-09-02 16:51:08 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class Demo3 {
+	static void Display() {
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Python");
+	}
+
+	public static void main(String[] args) {
+		Display();
+		Display();
+		Display();
+		Display();
+		// TODO Auto-generated method stub
+	}
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+package day_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Python");
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Python");
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Python");
+		System.out.println("Java");
+		System.out.println("Sql");
+		System.out.println("Python");
+		// TODO Auto-generated method stub
+
+	}
+
+}
